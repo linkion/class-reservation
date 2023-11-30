640baf31b07a381b526dd114d46dc8898888fc75
MiloszBrodziak <milosz.brodziak2004@gmail.com>
2023-11-29 21:24:25 -0600

    progress!!!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -63,9 +63,33 @@ fn reset_data() -> Status {
     Status::Ok
 }
 
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS)
         .mount("/", routes![index, reset_data])
         .mount("/", routes_rooms::routes())
         .mount("/", routes_students::routes())
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,4 +1,7 @@
 use yew::prelude::*;
+use serde::Deserialize;
+use gloo_net::http::Request;
+
 
 struct Class {
     id: i32,
@@ -8,8 +11,58 @@ struct Class {
     teacher: String,
 }
 
+#[derive(Deserialize, Debug, Clone)]
+pub struct DormJSON {
+    pub id: i32,
+    pub dorm_name: String,
+    pub dorm_group: String,
+    pub rooms: i32,
+    pub rooms_available: i32,
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    
+    let dorms_json = use_state(|| vec![]);
+    {
+        let dorms_json = dorms_json.clone();
+        use_effect_with((), move |_| {
+            let dorms_json = dorms_json.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let fetched_dorms_json: Vec<DormJSON> = Request::get("http://localhost:8081/dorms")
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+                dorms_json.set(fetched_dorms_json);
+            });
+            || ()
+        });
+    }/* 
+
+    let rooms_json = use_state(|| vec![]);
+    {
+        let rooms_json = rooms_json.clone();
+        use_effect_with((), move |_| {
+            let rooms_json = rooms_json.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let fetched_rooms_json: Vec<DormJSON> = Request::get("http://localhost:8081/rooms")
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+                rooms_json.set(fetched_rooms_json);
+            });
+            || ()
+        })
+    } */
+
+    let dorms : Vec<DormJSON> = dorms_json.to_vec();
+
     let classes = vec![
         Class {
             id: 1,
@@ -20,7 +73,7 @@ fn app() -> Html {
         },
         Class {
             id: 2,
-            class_name: "Calculus III".to_string(),
+            class_name: "Calculus II".to_string(),
             max_students: 200,
             registered_students: 89,
             teacher: "Jeremiah Heller".to_string(),
@@ -29,12 +82,16 @@ fn app() -> Html {
     let classes = classes.iter().map(|class| html! {
         <p key={class.id}>{format!("{} . . . . . . {} . . . . . . {} . . . . . . {}", class.teacher, class.class_name, class.max_students.to_string(), class.registered_students.to_string())}</p>
     }).collect::<Html>();
+    let dorms = dorms.iter().map(|dorm| html! {
+        <p key={dorm.id}>{format!("{} . . . . . . {}", dorm.dorm_name, dorm.dorm_group)}</p>
+    }).collect::<Html>();
     html! {
         <>
             <h1>{ "UIUC Class Reservation" }</h1>
             <div>
                 <h3>{"Classes: "}</h3>
-                {classes}
+                {dorms}
+                <button>{"Hi!"}</button>
             </div>
         </>
     }
